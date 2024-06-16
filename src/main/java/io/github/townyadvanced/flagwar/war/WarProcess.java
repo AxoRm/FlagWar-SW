c5a96327f88be0e3ceb9c475a8bb6c29c72ef423
AxoRm <raksenov50@gmail.com>
2024-06-16 20:14:48 +0300

    WarProcess.java

--- src/main/java/io/github/townyadvanced/flagwar/war/WarProcess.java
+++ src/main/java/io/github/townyadvanced/flagwar/war/WarProcess.java
@@ -1,5 +1,10 @@
 package io.github.townyadvanced.flagwar.war;
 
+import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.event.actions.TownyExplodingBlocksEvent;
+import com.palmergames.bukkit.towny.event.damage.TownyExplosionDamagesEntityEvent;
+import com.palmergames.bukkit.towny.event.damage.TownyPlayerDamageEntityEvent;
 import com.palmergames.bukkit.towny.event.damage.TownyPlayerDamagePlayerEvent;
 import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Town;
@@ -8,10 +13,15 @@ import com.palmergames.bukkit.towny.object.WorldCoord;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
@@ -26,17 +36,22 @@ public class WarProcess implements Listener {
     Town defenderTown;
     List<Resident> aggressors;
     List<Resident> defenders;
-    List<Resident>
+    List<Resident> participants;
     Set<Chunk> warChunks;
     Set<Chunk> sideWarChunks;
+    Set<Chunk> allChunks;
 
     public WarProcess(Town aggressorTown, Town defenderTown) {
         this.defenderTown = defenderTown;
         this.aggressorTown = aggressorTown;
-        this.aggressors = aggressorTown.getResidents();
-        this.defenders = defenderTown.getResidents();
+        this.aggressors = new ArrayList<>(aggressorTown.getResidents());
+        this.defenders = new ArrayList<>(defenderTown.getResidents());
+        this.participants = new ArrayList<>();
+        this.participants.addAll(aggressors);
+        this.participants.addAll(defenders);
         this.warChunks = new HashSet<>();
         this.sideWarChunks = new HashSet<>();
+        this.allChunks = new HashSet<>();
 
         // Add all chunks of the defender town to warChunks
         Set<WorldCoord> townCoords = new HashSet<>();
@@ -49,7 +64,7 @@ public class WarProcess implements Listener {
             }
         }
 
-        // Add chunks around the town that are within 2 chunks distance
+        // Add chunks around the town that are within 2 chunks distance to sideWarChunks
         for (WorldCoord coord : townCoords) {
             World world = Bukkit.getWorld(coord.getWorldName());
             if (world != null) {
@@ -65,13 +80,68 @@ public class WarProcess implements Listener {
             }
         }
 
+        // Combine warChunks and sideWarChunks into allChunks
+        allChunks.addAll(warChunks);
+        allChunks.addAll(sideWarChunks);
     }
     //EVENTS
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onAttackEvent(TownyPlayerDamagePlayerEvent damageEvent) {
+        Town attackerTown = damageEvent.getAttackerTown();
+        Town victimTown = damageEvent.getVictimTown();
+        if (attackerTown == null || victimTown == null) return;
         Resident attackingResident = damageEvent.getAttackingResident();
         Resident victimResident = damageEvent.getVictimResident();
-        if
+        if (attackingResident == null || victimResident == null) return;
+        if (participants.contains(attackingResident) && participants.contains(victimResident)) {
+            if (attackerTown.equals(victimTown)) {
+                damageEvent.setCancelled(true);
+                return;
+            }
+            Player attacker = attackingResident.getPlayer();
+            if (attacker == null) return;
+            if (allChunks.contains(attacker.getChunk())) {
+                damageEvent.setCancelled(false);
+            }
+        }
     }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onAttackEvent(TownyPlayerDamageEntityEvent damageEvent) {
+        Player attacker = damageEvent.getAttackingPlayer();
+        if (attacker == null) return;
+        if (participants.contains(TownyAPI.getInstance().getResident(attacker))) {
+            if (allChunks.contains(damageEvent.getEntity().getChunk())) {
+                damageEvent.setCancelled(false);
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onExplosionDamagesEvent(TownyExplosionDamagesEntityEvent explosionDamagesEvent) {
+        if (!explosionDamagesEvent.isCancelled()) return;
+        Chunk chunk = explosionDamagesEvent.getLocation().getChunk();
+        if (allChunks.contains(chunk)) explosionDamagesEvent.setCancelled(false);
+    }
+
+    @EventHandler
+    public void onRespawnEvent(PlayerRespawnEvent respawnEvent) {
+        Resident resident = TownyAPI.getInstance().getResident(respawnEvent.getPlayer());
+        if (!participants.contains(resident)) return;
+
+    }
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent respawnEvent) {
+        Resident resident = TownyAPI.getInstance().getResident(respawnEvent.getPlayer());
+        if (!participants.contains(resident)) return;
+
+    }
+
+    @EventHandler
+    public void onPlayerDeathEvent(PlayerDeathEvent deathEvent {
+
+    }
+
 }
